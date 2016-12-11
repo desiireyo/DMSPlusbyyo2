ce402ba406fc9a16d9720e1ab8c87ae450fb3bee
desiireyo <design.yo@gmail.com>
2016-12-11 18:18:53 +0700

    update
    update xxxx

--- js/search.js
+++ js/search.js
@@ -109,10 +109,12 @@
         datafields = [
             { name: 'PAYCODE', type: 'string' },
             { name: 'PAYDESC', type: 'string' },
+            { name: 'ACCMSTCOD', type: 'string' },
         ];
         fields = [
             { text: 'รหัสชำระโดย', datafield: 'PAYCODE', minwidth: 100,width: 250 },
             { text: 'คำอธิบาย', datafield: 'PAYDESC', minwidth: 200, width: 250 },
+            { text: 'รหัสบัญชี', datafield: 'ACCMSTCOD', minwidth: 200, width: 250 },
         ];
         keyno = 'PAYCODE';
     }
@@ -205,6 +207,184 @@
             { text: 'คำอธิบาย', datafield: 'TYPDESC', minwidth: 200, width: 250 },
         ];
         keyno = 'TYPCODE';
+    }
+    else if (show === "typhold") {
+        datafields = [
+            { name: 'HOLDCOD', type: 'string' },
+            { name: 'HOLDESC', type: 'string' },
+        ];
+        fields = [
+            { text: 'รหัสสาเหตุการยึด', datafield: 'HOLDCOD', minwidth: 100,width: 250 },
+            { text: 'คำอธิบาย', datafield: 'HOLDESC', minwidth: 200, width: 250 },
+        ];
+        keyno = 'HOLDCOD';
+    }
+    else if (show === "typlost") {
+        datafields = [
+            { name: 'LOSTCOD', type: 'string' },
+            { name: 'LOSTDESC', type: 'string' },
+        ];
+        fields = [
+            { text: 'รหัสสาเหตุหนี้สูญ', datafield: 'LOSTCOD', minwidth: 100,width: 250 },
+            { text: 'คำอธิบาย', datafield: 'LOSTDESC', minwidth: 200, width: 250 },
+        ];
+        keyno = 'LOSTCOD';
+    }
+    else if (show === "argroup") {
+        datafields = [
+            { name: 'ARGCOD', type: 'string' },
+            { name: 'ARGDES', type: 'string' },
+        ];
+        fields = [
+            { text: 'รหัสสาเหตุหนี้สูญ', datafield: 'ARGCOD', minwidth: 100,width: 250 },
+            { text: 'คำอธิบาย', datafield: 'ARGDES', minwidth: 200, width: 250 },
+        ];
+        keyno = 'ARGCOD';
+    }
+    else if (show === "setgrade") {
+        datafields = [
+            { name: 'GRDCOD', type: 'string' },
+            { name: 'GRDDES', type: 'string' },
+        ];
+        fields = [
+            { text: 'รหัสกลุ่มอาชีพ', datafield: 'GRDCOD', minwidth: 100,width: 250 },
+            { text: 'คำอธิบาย', datafield: 'GRDDES', minwidth: 200, width: 250 },
+        ];
+        keyno = 'GRDCOD';
+    }
+    else if (show === "setaump") {
+        datafields = [
+            { name: 'AUMPCOD', type: 'string' },
+            { name: 'AUMPDES', type: 'string' },
+        ];
+        fields = [
+            { text: 'รหัสอำเภอ', datafield: 'AUMPCOD', minwidth: 100,width: 250 },
+            { text: 'คำอธิบาย', datafield: 'AUMPDES', minwidth: 200, width: 250 },
+        ];
+        keyno = 'AUMPCOD';
+    }
+    else if (show === "setprov") {
+        datafields = [
+            { name: 'PROVCOD', type: 'string' },
+            { name: 'PROVDES', type: 'string' },
+        ];
+        fields = [
+            { text: 'รหัสจังหวัด', datafield: 'PROVCOD', minwidth: 100,width: 250 },
+            { text: 'คำอธิบาย', datafield: 'PROVDES', minwidth: 200, width: 250 },
+        ];
+        keyno = 'PROVCOD';
+    }
+    else if (show === "accmst") {
+        datafields = [
+            { name: 'ACCMSTCOD', type: 'string' },
+            { name: 'ACCMSTNAM', type: 'string' },
+        ];
+        fields = [
+            { text: 'รหัสบัญชี', datafield: 'ACCMSTCOD', minwidth: 100,width: 250 },
+            { text: 'คำอธิบาย', datafield: 'ACCMSTNAM', minwidth: 200, width: 250 },
+        ];
+        keyno = 'ACCMSTCOD';
+    }
+    else if (show === "tsale") {
+        datafields = [
+            { name: 'TSALE', type: 'string' },
+            { name: 'DESC', type: 'string' },
+        ];
+        fields = [
+            { text: 'ประเภทการขาย', datafield: 'TSALE', minwidth: 100,width: 250 },
+            { text: 'คำอธิบาย', datafield: 'DESC', minwidth: 200, width: 250 },
+        ];
+        keyno = 'TSALE';
+    }
+    else if (show === "setbank") {
+        datafields = [
+            { name: 'BANKCOD', type: 'string' },
+            { name: 'BANKNAM', type: 'string' },
+        ];
+        fields = [
+            { text: 'รหัสธนาคาร', datafield: 'BANKCOD', minwidth: 100,width: 250 },
+            { text: 'คำอธิบาย', datafield: 'BANKNAM', minwidth: 200, width: 250 },
+        ];
+        keyno = 'BANKCOD';
+    }
+    else if (show === "bankbook") {
+        datafields = [
+            { name: 'BANKBOOKCOD', type: 'string' },
+            { name: 'BOOKNO', type: 'string' },
+            { name: 'BOOKNAM', type: 'string' },
+        ];
+        fields = [
+            { text: 'รหัส', datafield: 'BANKBOOKCOD', minwidth: 100,width: 250 },
+            { text: 'รหัสบัญชีธนาคาร', datafield: 'BOOKNO', minwidth: 200, width: 250 },
+            { text: 'ชื่อบัญชี', datafield: 'BOOKNAM', minwidth: 200, width: 250 },
+        ];
+        keyno = 'BANKBOOKCOD';
+    }
+    else if (show === "banklocat") {
+        datafields = [
+            { name: 'BANKLOCCOD', type: 'string' },
+            { name: 'BANKLOCNAM', type: 'string' },
+        ];
+        fields = [
+            { text: 'รหัส', datafield: 'BANKLOCCOD', minwidth: 100,width: 250 },
+            { text: 'คำอธิบาย', datafield: 'BANKLOCNAM', minwidth: 200, width: 250 },
+        ];
+        keyno = 'BANKLOCCOD';
+    }
+    else if (show === "bkbooktyp") {
+        datafields = [
+            { name: 'BOOKTYPCOD', type: 'string' },
+            { name: 'BOOKTYPNAM', type: 'string' },
+        ];
+        fields = [
+            { text: 'รหัส', datafield: 'BOOKTYPCOD', minwidth: 100,width: 250 },
+            { text: 'คำอธิบาย', datafield: 'BOOKTYPNAM', minwidth: 200, width: 250 },
+        ];
+        keyno = 'BOOKTYPCOD';
+    }
+    else if (show === "rtchq") {
+        datafields = [
+            { name: 'RTCODE', type: 'string' },
+            { name: 'RTNAME', type: 'string' },
+        ];
+        fields = [
+            { text: 'รหัส', datafield: 'RTCODE', minwidth: 100,width: 250 },
+            { text: 'คำอธิบาย', datafield: 'RTNAME', minwidth: 200, width: 250 },
+        ];
+        keyno = 'RTCODE';
+    }
+    else if (show === "paydue") {
+        datafields = [
+            { name: 'DUECODE', type: 'string' },
+            { name: 'DUEDESC', type: 'string' },
+        ];
+        fields = [
+            { text: 'รหัส', datafield: 'DUECODE', minwidth: 100,width: 250 },
+            { text: 'คำอธิบาย', datafield: 'DUEDESC', minwidth: 200, width: 250 },
+        ];
+        keyno = 'DUECODE';
+    }
+    else if (show === "setacti") {
+        datafields = [
+            { name: 'ACTICODE', type: 'string' },
+            { name: 'ACTIDESC', type: 'string' },
+        ];
+        fields = [
+            { text: 'รหัส', datafield: 'ACTICODE', minwidth: 100,width: 250 },
+            { text: 'คำอธิบาย', datafield: 'ACTIDESC', minwidth: 200, width: 250 },
+        ];
+        keyno = 'ACTICODE';
+    }
+    else if (show === "setreson") {
+        datafields = [
+            { name: 'RESONCD', type: 'string' },
+            { name: 'RESNDES', type: 'string' },
+        ];
+        fields = [
+            { text: 'รหัส', datafield: 'RESONCD', minwidth: 100,width: 250 },
+            { text: 'คำอธิบาย', datafield: 'RESNDES', minwidth: 200, width: 250 },
+        ];
+        keyno = 'RESONCD';
     }
     fields.unshift({
         text: 'ลำดับ',
@@ -303,6 +483,54 @@
         }
         else if (show === "typcont") {
             sqltxt = "SELECT * FROM TYPCONT WHERE UPPER(TYPCODE||TYPDESC) LIKE '%"+param1+"%' ORDER BY TYPDESC";
+        }
+        else if (show === "typhold") {
+            sqltxt = "SELECT * FROM TYPHOLD WHERE UPPER(HOLDCOD||HOLDESC) LIKE '%"+param1+"%' ORDER BY HOLDCOD";
+        }
+        else if (show === "typlost") {
+            sqltxt = "SELECT * FROM TYPLOST WHERE UPPER(LOSTCOD||LOSTDESC) LIKE '%"+param1+"%' ORDER BY LOSTCOD";
+        }
+        else if (show === "argroup") {
+            sqltxt = "SELECT * FROM ARGROUP WHERE UPPER(ARGCOD||ARGDES) LIKE '%"+param1+"%' ORDER BY ARGCOD";
+        }
+        else if (show === "setgrade") {
+            sqltxt = "SELECT * FROM SETGRADE WHERE UPPER(GRDCOD||GRDDES) LIKE '%"+param1+"%' ORDER BY GRDCOD";
+        }
+        else if (show === "setaump") {
+            sqltxt = "SELECT * FROM SETAUMP WHERE UPPER(AUMPCOD||AUMPDES) LIKE '%"+param1+"%' ORDER BY AUMPCOD";
+        }
+        else if (show === "setprov") {
+            sqltxt = "SELECT * FROM SETPROV WHERE UPPER(PROVCOD||PROVDES) LIKE '%"+param1+"%' ORDER BY PROVCOD";
+        }
+        else if (show === "accmst") {
+            sqltxt = "SELECT * FROM ACCMST WHERE UPPER(ACCMSTCOD||ACCMSTNAM) LIKE '%"+param1+"%' ORDER BY ACCMSTCOD";
+        }
+        else if (show === "tsale") {
+            sqltxt = "SELECT * FROM TSALE WHERE UPPER(TSALE||DESC) LIKE '%"+param1+"%' ORDER BY TSALE";
+        }
+        else if (show === "setbank") {
+            sqltxt = "SELECT * FROM SETBANK WHERE UPPER(BANKCOD||BANKNAM) LIKE '%"+param1+"%' ORDER BY BANKCOD";
+        }
+        else if (show === "bankbook") {
+            sqltxt = "SELECT * FROM BANKBOOK WHERE UPPER(BANKBOOKCOD||BOOKNO) LIKE '%"+param1+"%' ORDER BY BANKBOOKCOD";
+        }
+        else if (show === "banklocat") {
+            sqltxt = "SELECT * FROM BANKLOCAT WHERE UPPER(BANKLOCCOD||BANKLOCNAM) LIKE '%"+param1+"%' ORDER BY BANKLOCCOD";
+        }
+        else if (show === "bkbooktyp") {
+            sqltxt = "SELECT * FROM BKBOOKTYPE WHERE UPPER(BOOKTYPCOD||BOOKTYPNAM) LIKE '%"+param1+"%' ORDER BY BOOKTYPCOD";
+        }
+        else if (show === "rtchq") {
+            sqltxt = "SELECT * FROM RTCHQ WHERE UPPER(RTCODE||RTNAME) LIKE '%"+param1+"%' ORDER BY RTCODE";
+        }
+        else if (show === "paydue") {
+            sqltxt = "SELECT * FROM PAYDUE WHERE UPPER(DUECODE||DUEDESC) LIKE '%"+param1+"%' ORDER BY DUECODE";
+        }
+        else if (show === "setacti") {
+            sqltxt = "SELECT * FROM SETACTI WHERE UPPER(ACTICODE||ACTIDESC) LIKE '%"+param1+"%' ORDER BY ACTICODE";
+        }
+        else if (show === "setreson") {
+            sqltxt = "SELECT * FROM SETRESON WHERE UPPER(RESONCD||RESNDES) LIKE '%"+param1+"%' ORDER BY RESONCD";
         }
 
         // Qurey Data
